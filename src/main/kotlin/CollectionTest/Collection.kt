ae03cf4ae4df57c1c7528b3fcdcd2ea5fe362759
hasihime <osb330@naver.com>
2022-04-17 23:13:37 +0900

    make joinToString fo

--- src/main/kotlin/CollectionTest/Collection.kt
+++ src/main/kotlin/CollectionTest/Collection.kt
@@ -1,15 +1,25 @@
 package CollectionTest
+import CollectionTest.joinToString
 
 
+
+fun reportOpertioncount(){
+    println("Operation performed $opCount times")
+}
 fun main(args: Array<String>)  {
     val set = arrayListOf(1, 5, 6)
     val map = hashMapOf(1 to "one", 7 to "seven", 53 to "fifty-three")
 
     val strings = listOf("zip", "gago", "sipda")
-
+    reportOpertioncount()
     println(strings.last())
     println(set.maxOrNull())
     println(map)
+    println(joinToString(strings))
+    reportOpertioncount()
+
+
+
 
 
 }
---
+++ src/main/kotlin/CollectionTest/joinToString.kt
@@ -0,0 +1,43 @@
+package CollectionTest
+
+
+var opCount=0
+
+fun perfomOpertion(){
+    opCount++
+}
+
+fun <T> joinToString(
+    /*
+    collection: Collection<T>,
+    separator: String,
+    prefix: String,
+    postfix: String
+     */
+    collection: Collection<T>,
+    separator: String = ".",
+    prefix: String = "",
+    postfix: String = ""
+): String {
+    perfomOpertion()
+    val result = StringBuilder(prefix)
+    for ((index, element) in collection.withIndex()) {
+        if (index > 0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun main(args: Array<String>) {
+    val list = listOf(1, 2, 3)
+    println(joinToString(list, ";", "(", ")"))
+//    이걸 이렇게 표현도 가능
+    println(joinToString(collection = list, separator = ";", prefix = "(", postfix = ")"))
+    // separator, prefix, postfix 생략 가능
+    println(joinToString(list ))
+    //separator만 지정
+    println(joinToString(list ,";"))
+    // 코틀린은 순서 상관없이 가능
+    println(joinToString(collection = list ,prefix = "(", postfix = ")"))
+}
